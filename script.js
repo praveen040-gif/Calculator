36ad453b5c4f27b339661230b2801f00ae08c16a
Gangisetty Durga Praveen <gangisettydurgapraveen2002@gmail.com>
2024-11-13 23:14:35 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,49 @@
+let buttons=document.getElementsByTagName('button');
+let result=document.getElementById('result');
+console.log(result)
+console.log(buttons)
+for(i=0;i<buttons.length;i++)
+{
+    buttons[i].onclick=function()
+    {
+        let input=this.innerText;
+        updateDisplay(input)
+    }
+}
+function updateDisplay(input)
+{
+    let currentdisplay=result.innerText;
+    if(currentdisplay==0)
+    {
+        if(input !='DEL' && input!='=' && input!='C')
+        {
+            result.innerText=""
+            result.innerText=input
+        }
+    }
+    else{
+        if(input=="DEL")
+        {
+            result.innerText=currentdisplay.substring(0,currentdisplay.length-1);
+            if(result.innerText=="")
+            {
+                result.innerText="0";
+            }
+        }
+        else if(input=="C")
+        {
+            result.innerText=0;
+        }
+        else if(input !='DEL' && input!='=' && input!='C')
+            {
+                result.innerText+=input
+            }
+        else if(input=="=")
+        {
+            let display=result.innerText;
+            result.innerText=eval(display);
+        }
+        
+    }
+
+}
